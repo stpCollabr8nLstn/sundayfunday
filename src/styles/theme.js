9d2947dd9f266a7c795a29fc6122661fc2892a0c
stpCollabr8nLstn <amrios2817@gmail.com>
2020-02-16 14:03:50 -0500

    setup styled components

---
+++ src/styles/theme.js
@@ -0,0 +1,10 @@
+export const colors = {
+  main: 'rebeccapurple'
+}
+
+// https://openframework.stanford.edu/guide/media-queries-code
+export const breakpoints = {
+  mobileOnly: 'max-width: 480px',
+  tabletDown: 'max-width: 979px',
+  desktopUp: 'min-width: 980px'
+}
